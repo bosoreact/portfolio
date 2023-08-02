2395fe6d7788bb191e902f8c9261de137377abab
Bogdan <andrewbagio@yahoo.com>
2023-08-02 11:47:16 +0100

    Chaning Ico, adding scss, adding exceptions to gitignore,modifiing global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 import  Main  from './pages/main'
 
 export default function Home() {
--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -1,5 +1,31 @@
-export default function Main(){
+import style from "../styles/main.module.css"
 
-
-    return <div>Main Page</div>
+export default function Main() {
+  return (
+    <div className={style["main-page"]}>
+      <div className={style["Top"]}>nav</div>
+      <div className={style["box-wrapper"]}>
+        <div className={style["box-line"]}>
+          <div className={style["greetings-box"]}>picture and hello</div>
+          <div className={style["right-nested-box"]}>
+            <div className={style["moving-phrase"]}>moving phrase</div>
+            <div className={style["credentials-and-portfolio"]}>
+              <div className={style["c-and-p-box"]}>1</div>
+              <div className={style["c-and-p-box"]}>portfolio</div>
+            </div>
+          </div>
+        </div>
+        <div className={style["box-line-s"]}>
+            <div className={style["cv-box"]}>cv box</div>
+            <div className={style["offered-services-box"]}>services</div>
+            <div className={style["profiles-box"]}>profile</div>
+        </div>
+        <div className={style["box-line-s"]}>
+            <div className={style["statistic-and-notes"]}>statistic</div>
+            <div className={style["statistic-and-notes"]}>note</div>
+        </div>
+      </div>
+      <div className={style["Bottom"]}>bottom</div>
+    </div>
+  );
 }
