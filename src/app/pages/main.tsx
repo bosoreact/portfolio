d996f6275e9ad589ba01f8f1c810e89def450de4
Bogdan <andrewbagio@yahoo.com>
2023-08-04 15:59:40 +0100

    graphic added, main refactored, main module refactored, added: -my work, -CV -services

--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -1,16 +1,42 @@
 import style from "../styles/main.module.css";
 import { portfolio } from "../../../public/portfolio";
 import photo from "../../../images/photo.png";
-import Image from "next/image";
+import cvImage from "../../../images/download_image/128x128.png";
+import Image, { StaticImageData } from "next/image";
+import dbImage from "../../../images/database/128x128.png"
+import codingImage from "../../../images/coding/128x128.png"
+import uiImage from "../../../images/ui/128x128.png"
+import cloudImage from "../../../images/servers/cloud-data.png"
+import path from "path";
 
-const myPhoto = (
-  <Image
-    src={photo}
-    className={style["photo-style"]}
-    alt="Picture of the author"
-  />
-);
+const ShowImage = (imageSrc: StaticImageData) => {
+  return (
+    <Image
+      src={imageSrc}
+      className={style["photo-style"]}
+      alt="Picture of the author"
+    />
+  );
+};
 export default function Main() {
+  const routeLink = (
+    <div className={style["route-link"]}>
+      <div className={style["arrow"]}></div>
+      <div className={style["arrow"]}></div>
+      <div className={style["arrow"]}></div>
+    </div>
+  );
+  const descriptionAndLink = (p1: string, p2: string) => {
+    return (
+      <div className={style["description-and-link"]}>
+        <div className={style["description"]}>
+          <p>{p1}</p>
+          <p>{p2}</p>
+        </div>
+        {routeLink}
+      </div>
+    );
+  };
   return (
     <div className={style["main-page"]}>
       <div className={style["Top"]}>
@@ -27,7 +53,7 @@ export default function Main() {
       <div className={style["box-wrapper"]}>
         <div className={style["box-line"]}>
           <div className={style["greetings-box"]}>
-            <div className={style["image-wraper"]}>{myPhoto}</div>
+            <div className={style["image-wraper"]}> {ShowImage(photo)}</div>
             <div className={style["greeting-note"]}>
               <p>{portfolio.description.p1}</p>
               <p>{portfolio.description.p2}</p>
@@ -43,25 +69,29 @@ export default function Main() {
             <div className={style["credentials-and-portfolio"]}>
               <div className={style["c-and-p-box"]}>
                 <div className={style["top-sign"]}>{portfolio.sign}</div>
-                <div className={style["c-description-and-link"]}>
-                  <div className={style["credentials-description"]}>
-                    <p>MORE ABOUT ME</p>
-                    <p>Credentials</p>
-                  </div>
-                  <div className={style["route-link"]}>
-                    <div className={style["arrow"]}></div>
-                    <div className={style["arrow"]}></div>
-                    <div className={style["arrow"]}></div>
-                  </div>
-                </div>
+                {descriptionAndLink("MORE ABOUT ME", "Credentials")}
               </div>
-              <div className={style["c-and-p-box"]}>portfolio</div>
+              <div className={style["c-and-p-box"]}>
+                <div className={style["bg-info"]}>MY WORK</div>
+                {descriptionAndLink("SHOWCASE", "Portfolio")}
+              </div>
             </div>
           </div>
         </div>
         <div className={style["box-line-s"]}>
-          <div className={style["cv-box"]}>cv box</div>
-          <div className={style["offered-services-box"]}>services</div>
+          <div className={style["cv-box"]}>
+            <div className={style["image-wraper"]}>{ShowImage(cvImage)}</div>
+            {descriptionAndLink("MY CV", "Download")}
+          </div>
+          <div className={style["offered-services-box"]}>
+            <div className={style["image-wraper"]}>
+              {ShowImage(dbImage)}
+              {ShowImage(codingImage)}
+              {ShowImage(uiImage)}
+              {ShowImage(cloudImage)}
+            </div>
+            {descriptionAndLink("SPECIALIZATION", "Services Offering")}
+          </div>
           <div className={style["profiles-box"]}>profile</div>
         </div>
         <div className={style["box-line-s"]}>
