fdfd440ea4dc8709def127e9b53a90c442bfdc41
Bogdan <andrewbagio@yahoo.com>
2023-08-30 17:18:41 +0100

    adding separated components for footer and top bar adding route for credentials

---
+++ src/app/components/afoota.tsx
@@ -0,0 +1,18 @@
+import { portfolio } from "../../../public/portfolio";
+import style from "../styles/main.module.css";
+
+export default function Afoota(){
+
+    return (
+        <div className={style["Bottom"]}>
+        <div className={style["placement"]}>{portfolio.CompanyName}</div>
+        <div className={style["navigation-buttons"]}>
+          <div className={style["nav-button"]}>navbutton</div>
+          <div className={style["nav-button"]}>navbutton</div>
+          <div className={style["nav-button"]}>navbutton</div>
+          <div className={style["nav-button"]}>navbutton</div>
+          <div className={style["nav-button"]}>navbutton</div>
+        </div>
+      </div>
+    )
+}
