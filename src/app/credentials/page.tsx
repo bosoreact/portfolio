63b03aec309ead38c99e5fe13b8fb38870dcb6fc
Bogdan <andrewbagio@yahoo.com>
2023-09-04 22:31:13 +0100

    styles file name changed, small adjstments added

--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -2,34 +2,38 @@ import Afoota from "../components/afoota";
 import TopBar from "../components/top-bar";
 import style from "@/app/styles/main.module.css";
 import { portfolio } from "../../../public/portfolio";
+// importing credentials styles
+import c_style from "@/app/credentials/credentials.module.css";
 
 export default function Page() {
   return (
     <div className={style["main-page"]}>
       <TopBar />
-      <div className={style["left-side-credentials"]}>
-        <div className={style["photo-and-profile"]}>
-          <div className={style["image-wrapper"]}>
-            <div className={style["photo-style"]}>photo</div>
+      <div className={c_style["box-wrapper"]}>
+        <div className={c_style["left-side-credentials"]}>
+          <div className={c_style["photo-and-profile"]}>
+            <div className={c_style["image-wrapper"]}>
+              <div className={c_style["photo-c_style"]}>photo</div>
+            </div>
+            <div className={c_style["profile-note"]}>
+              <p>name</p>
+              <p>{portfolio.domain && portfolio.domain}</p>
+            </div>
+            <div className={c_style["profiles-box"]}> twitter linkenin</div>
+            <div className={c_style["contact-button"]}>contact me</div>
           </div>
-          <div className={style["profile-note"]}>
-            <p>name</p>
-            <p>website @surname as a link</p>
+          {/* recomendation box will optional and will depend from available file on server */}
+          <div className={c_style["recomendation-box"]}>
+            <div className={c_style["image-wrapper"]}>
+              <div className={c_style["photo-c_style"]}>recomendation</div>
+            </div>
           </div>
-          <div className={style["profiles-box"]}> twitter linkenin</div>
-          <div className={style["contact-button"]}>contact me</div>
         </div>
-        {/* recomendation box will optional and will depend from available file on server */}
-        <div className={style["recomendation-box"]}>
-          <div className={style["image-wrapper"]}>
-            <div className={style["photo-style"]}>recomendation</div>
-          </div>
+        <div className={c_style["right-side-credentials"]}>
+          <div className={c_style["education-box"]}>education</div>
+          <div className={c_style["skills-box"]}>skills</div>
+          <div className={c_style["badges-and-awards"]}>badges and awards</div>
         </div>
-      </div>
-      <div className={style["right-side-credentials"]}>
-      <div className={style["education-box"]}>education</div>
-      <div className={style["skills-box"]}>skills</div>
-      <div className={style["badges-and-awards"]}>badges and awards</div>
       </div>
       <Afoota />
     </div>
