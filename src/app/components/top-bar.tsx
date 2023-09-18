9942ff9e1d7b53409dd18a030b765311b986c913
Szymon Basiul <szymon.basiul@wp.pl>
2023-09-18 22:05:31 +0200

    navbar version implemented, updated body margins

--- src/app/components/top-bar.tsx
+++ src/app/components/top-bar.tsx
@@ -2,18 +2,41 @@ import { portfolio } from "@/app/portfolio";
 import style from "../styles/main.module.css";
 import modeImg from "../../../images/dark_mode.svg";
 import { ShowImage } from "../pages/main";
+import Link from "next/link";
 
 export default function TopBar() {
   return (
     <div className={style["Top"]}>
       <div className={style["left-placement"]}>{portfolio.CompanyName}</div>
+
       <div className={style["navigation-buttons"]}>
-        <div className={style["nav-button"]}>navbutton</div>
-        <div className={style["nav-button"]}>navbutton</div>
-        <div className={style["nav-button"]}>navbutton</div>
-        <div className={style["nav-button"]}>navbutton</div>
-        <div className={style["nav-button"]}>navbutton</div>
+        <div className={style["nav-button"]}>
+          <Link className={style["nav-button-link"]} href="/credentials">
+            <>Credentials</>
+          </Link>
+        </div>
+        <div className={style["nav-button"]}>
+          <Link className={style["nav-button-link"]} href="/portfolio">
+            <>Portfolio</>
+          </Link>
+        </div>
+        <div className={style["nav-button"]}>
+          <Link className={style["nav-button-link"]} href="/download">
+            <>Download</>
+          </Link>
+        </div>
+        <div className={style["nav-button"]}>
+          <Link className={style["nav-button-link"]} href="/services">
+            <>Services</>
+          </Link>
+        </div>
+        <div className={style["nav-button"]}>
+          <Link className={style["nav-button-link"]} href="/profiles">
+            <>Profiles</>
+          </Link>
+        </div>
       </div>
+
       <div className={style["right-placement"]}>
         <button className={style["theme-button"]}>
           <span className={style["text"]}> Dark mode </span>
