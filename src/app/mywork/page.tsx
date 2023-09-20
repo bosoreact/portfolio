d89a2ba8d8f6e10ae39655446a306b46c29a7c8b
Bogdan Todorow <andrewbagio@yahoo.com>
2023-09-20 19:34:46 +0100

    divs generating from given data

--- src/app/mywork/page.tsx
+++ src/app/mywork/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import TopBar from "../components/top-bar";
 import Afoota from "../components/afoota";
 import style from "@/app/styles/main.module.css";
@@ -5,53 +7,28 @@ import m_style from "@/app/mywork/mywork.module.css";
 import { MyWork } from "@/app/my-work";
 import exampleImage from "@/../images/my-work/figma_shoes_shop.png";
 import Image, { StaticImageData } from "next/image";
+import { resolve } from "path";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [mywork, setMywork] = useState(MyWork);
   /*here I will create function that will read mywork array of objects
   and depending on amount of objects will divide 1/3 obejct to left-side-mywork
   and 2/3 object to right-side-mywork. zero index must be at the begining of left-side-mywork.
   index one and two must be at the begining of right-side-mywork
-
-    */
+*/
   const divideMyworkOnTwoSides = () => {
-    console.log(MyWork)
-    let mywork = MyWork;
-    let left: object[] | any[] = [];
-    let right: object[] | any[] = [];
-    mywork.forEach((x) => {
-      left[left.length] = mywork.shift();
-      right[right.length] = mywork.shift();
-      right[right.length] = mywork.shift();
-    });
+    let tempwork = mywork;
+    let left = [];
+    let right = [];
+    for (let i = 0; i < mywork.length ; i++) {
+      if (tempwork.length > 0) left[left.length] = tempwork.shift();
+      if (tempwork.length > 0) right[right.length] = tempwork.shift();
+      if (tempwork.length > 0) right[right.length] = tempwork.shift();
+    }
+    console.log("left", Object.keys(left[0])[0],left.length);
+    console.log("right", right.length);
     return { left, right };
-  };
-  const extractMyWork = () => {
-    const { left, right } = divideMyworkOnTwoSides();
-    return (
-      <div className={m_style["box-wrapper"]}>
-        <div className={m_style["left-side-mywork"]}>
-          {left.map((index, x) => {
-            return (
-              <div key={index} className={m_style["project-box"]}>
-                {exampleProject}
-              </div>
-            );
-          })}
-        </div>
-        <div className={m_style["right-side-mywork"]}>
-          {right.map((index, x) => {
-            return (
-              <div key={index+10} className={m_style["project-box"]}>
-                {exampleProject}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  };
-  const projectBox = () => {
-    return MyWork.length;
   };
   const ShowImage = (imageSrc: StaticImageData) => {
     return (
@@ -65,11 +42,43 @@ export default function Page() {
   const exampleProject = (
     <div className={m_style["project-box"]}>{ShowImage(exampleImage)}</div>
   );
-  console.log(123, divideMyworkOnTwoSides());
+console.log(Object.keys(mywork[0])[0]);
+  const displayMywork = () => {
+    // console.log("twoSides",  divideMyworkOnTwoSides())
+    const { left, right } = divideMyworkOnTwoSides();
+    return (
+      <div className={m_style["box-wrapper"]}>
+        <div className={m_style["left-side-mywork"]}>
+          {left.map((x:any) => {
+            return (
+              <div key={Object.keys(x)[0]} className={m_style["project-box"]}>
+                {exampleProject}
+              </div>
+            );
+          })}
+        </div>
+        <div className={m_style["right-side-mywork"]}>
+          {right.map((x:any) => {
+            return (
+              <div key={Object.keys(x)[0]} className={m_style["project-box"]}>
+                {exampleProject}
+              </div>
+            );
+          })}
+          {<div className={m_style["project-box"]}>{exampleProject}</div>}
+        </div>
+      </div>
+    );
+  };
+
+  const projectBox = () => {
+    return mywork.length;
+  };
+
   return (
     <div className={style["main-page"]}>
       <TopBar />
-      {extractMyWork()}
+      {displayMywork()}
       {/* <div className={m_style["box-wrapper"]}>
         <div className={m_style["left-side-mywork"]}>
           <div className={m_style["project-box"]}>{exampleProject}</div>
