2ac1d91e4d2e89035abfee894db3e453f27e793e
Bogdan Todorow <andrewbagio@yahoo.com>
2023-09-30 18:54:00 +0100

    updating portfolio with available services

--- public/personal_data/portfolio_default.tsx
+++ public/personal_data/portfolio_default.tsx
@@ -56,6 +56,11 @@ export const portfolio = {
         "colaborators": "+35",
         "projects": "+14"
     },
+    "services": [{ "serviceName": "Description about offered skill/serviceLorem ipsum dolor nulla pariatur.Lorem Lorem ipsum" },
+    { "serviceName": "Description about offered skill/serviceLorem ipsum dolor nulla pariatur.Lorem Lorem ipsum" },
+    { "serviceName": "Description about offered skill/serviceLorem ipsum dolor nulla pariatur.Lorem Lorem ipsum" },
+    { "serviceName": "Description about offered skill/serviceLorem ipsum dolor nulla pariatur.Lorem Lorem ipsum" }
+    ],
     "aboutMe": {
         "short": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
         "long": ["Lorem ipsum dolor nulla pariatur.Lorem ipsum dolor nulla pariatur.", " Loreo laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."],
