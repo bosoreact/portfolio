1fdfc20c94a9e6025e4ab718313c998f2317bd2c
Bogdan Todorow <andrewbagio@yahoo.com>
2023-11-13 16:08:05 +0000

    creadted styles from about , updated div structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,13 +3,17 @@
 import style from "@/app/styles/main.module.css";
 import TopBar from "../components/top-bar";
 import Afoota from "../components/afoota";
-
+import about_style from "./about.module.css"
 export default function About() {
 
 
     return(<div className={style["main-page"]}>
             <TopBar />
-        <>About Under construction</>
+        <div className={about_style["box-wrapper"]}>
+        <div className={about_style["summary-and-photo"]}>summary and photo </div>
+        <div className={about_style["exp-and-education"]}>exp and education</div>
+        <div className={about_style["bottom-boxes"]}>bottom boxes</div>
+        </div>
         <Afoota />
     </div>
         )
