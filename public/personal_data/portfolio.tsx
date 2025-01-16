5b315977dd7cddfed0642d3e7d91a663aa1ba950
Bogdan Todorow <andrewbagio@yahoo.com>
2025-01-16 12:06:06 +0000

    sending my files

---
+++ public/personal_data/portfolio.tsx
@@ -0,0 +1,155 @@
+export const portfolio = {
+  CompanyName: "B.A.TODOROW",
+  domain: "bogdan@atodorow.com",
+  description: {
+    p1: "A SOFTWARE ENGINEER",
+    p2: "Bogdan Andrew Todorow",
+    p3: "I am a software engineer based in Inverness (Scottish Highlands)",
+  },
+  contact: {
+    email: "bogdan@atodorow.com",
+    number: "07746428153",
+    adress: ["Inverness, Highlands", "Scotland"],
+  },
+  socials: {
+    twitter: "https://twitter.com/BogdanTodorow",
+    linkedin: "https://www.linkedin.com/in/bogdan-todorow/",
+  },
+  scrolingText: "I Am Available For Contract or Freelance Work",
+  sign: "Todorow",
+  stats: {
+    experience: "04",
+    colaborators: "+45",
+    projects: "+28",
+  },
+  services: [
+    {
+      "DATABASE MANAGEMENT":
+        "I have experience in setting up, maintaining, and optimizing relational databases, such as MySQL, PostgreSQL, and SQLite. I have worked on CRUD operations, database design, and have a good understanding of database administration and performance tuning.",
+    },
+    {
+      "SOFTWARE ENGINEERING":
+        "I know how to build applications and Web applications using technologies like Python, C-sharp React, TypeScript, and Node.js. My skills encompass full-stack development, version control with Git, and collaborating in agile teams to deliver high-quality software products.",
+    },
+    {
+      "UI DESIGNING":
+        "I have a strong foundation in creating user-friendly and visually appealing interfaces. I am proficient in tools like Figma, allowing me to translate design concepts into compelling user experiences. My expertise enhances the overall user interface and experience of web applications.",
+    },
+    {
+      "DEVOPS SERVICES":
+        "I you have experience with server maintenance, ensuring the smooth operation of web applications. My skills include working with AWS, IBM, and Linux, along with SSH for secure access. These capabilities contribute to efficient deployment and maintenance of software solutions. This web app is deployed using CI/CD pipelines with Jenkins, streamlining the development and release process.",
+    },
+  ],
+  aboutMe: {
+    short:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    long: [
+      "I am a passionate software engineer who enjoys problem-solving and applying my skills quickly and effectively. ",
+      "I take pride in my strong technical and analytical skills. My creative mindset and positive attitude have led to numerous succesful projects.",
+    ],
+  },
+  experience: [
+    {
+      year: "05.2023 - 07.2023(2months)",
+      jobTitle: "IT Administrator/ Internship",
+      companyName: "Campbell’s of Beauly",
+      jobDescription:
+        "I was effectively communicating with stakeholders focusing on efficient time management. I was responsible for creating the API that retrieves information from WooCommerce plugin. I was utilizing multiple JavaScript libraries for data upload and manipulation. ",
+    },
+    {
+      year: "02.2023 – 2023.05 (3months)/ Internship ",
+      jobTitle: "IT Service Team member",
+      companyName: "UHI (University of the Highlands and Islands)",
+      jobDescription:
+        "Team focused on solving technical problems related to AWS, Azure, Active Directories, and databases at UHI. Deeper understanding of differences between technical problem-solving at commercial companies and government ones.",
+    },
+    {
+      year: "2019 – 2023 (3 years)",
+      jobTitle: "React Developer",
+      companyName: "Highland Specialists",
+      jobDescription:
+        "I joined Highland Specialists as a React developer. I progressed to a mid-level position and eventually became a team leader. I gained valuable experience working alongside other developers and I was focusing on delivering high quality products on time.",
+    },
+    {
+      year: "2018 - now (5years)",
+      jobTitle: "College and Private Projects",
+      companyName: "self-employed",
+      jobDescription:
+        "I learned how to work in a group, how to treat team members with respect. Over numerous projects, I used different programming languages such as Python, C#, C++, and JavaScript. I acquired knowledge of JavaScript most popular frameworks like Vue.js, React.js, Svelte.js, Nuxt.js, and Next.js. I contributed to the development of various projects and always tried to play a key role in the team's success.",
+    },
+  ],
+
+  education: [
+    {
+      year: "09.2021 - now (2 years) ",
+      school: "University of Highlands and Islands",
+      degree: "Bachelor of Sciences",
+      courseDescription: "Applied Software Development",
+    },
+    {
+      year: "09.2006 - 06.2010 (4 years)",
+      school: "Community college",
+      degree: "Technician,school diploma.",
+      courseDescription: "Software Development",
+    },
+  ],
+  skills: [
+    {
+      lvl: "Good",
+      skill: "JavaScript ",
+      shortDescription: "Experience with JavaScript libraries and OOP",
+    },
+    {
+      lvl: "Good",
+      skill: "React Typescript",
+      shortDescription: "Writing inferences, event types and more...",
+    },
+    {
+      lvl: "Good",
+      skill: "React.js",
+      shortDescription:
+        "Ability to build complex, interactive UIs in web and mobile applications",
+    },
+    {
+      lvl: "excelent",
+      skill: "Git",
+      shortDescription:
+        "Strong understanding of Git commands, working on branches, and resolving conflicts.",
+    },
+  ],
+  awardsAndBadges: [
+    {
+      year: "Sep 2022",
+      name: "Agile Explorer",
+      shortDescription:
+        "Foundational understanding of the Agile values, principles.",
+    },
+    {
+      year: "Apr 2022",
+      name: "Amazon Web Services",
+      shortDescription: "Serverless Computing with AWS Lambda",
+    },
+    {
+      year: "Nov 2021",
+      name: "RESTful APIs",
+      shortDescription:
+        "Designing, building RESTful APIs and API design patterns",
+    },
+    {
+      year: "Nov 2021",
+      name: "Node.js: Security",
+      shortDescription: "Securing apps at all level, tools for testing",
+    },
+    {
+      year: "Oct 2022",
+      name: "Java 17",
+      shortDescription: "Data types, strings, functions, and loops.",
+    },
+    {
+      year: "Feb 2021",
+      name: "React.js",
+      shortDescription:
+        "Demonstrable knowledge of React.js, with a deep understanding of its principles.",
+    },
+  ],
+};
